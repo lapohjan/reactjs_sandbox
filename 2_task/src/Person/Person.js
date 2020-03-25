b169e24fee33b4669795648f726554d6c220ac1d
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-03-25 13:24:47 +0200

    created person component

---
+++ 2_task/src/Person/Person.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Person.css";
+
+const Person = () => {
+    return (<div className="card">
+        <p>Hello, my name is Laura and I am 42 years old.</p>
+        </div>);
+};
+
+export default Person;
