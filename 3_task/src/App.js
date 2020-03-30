12dc35ffc757fc9ec005d8c7d8763c053f75d9a4
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-03-30 11:00:50 +0300

    added state

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -3,10 +3,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    likes: 100
+  };
+
   render() {
     return (
       <div>
-        <h1>Total likes: </h1>
+        <h1>Total likes: {this.state.likes} </h1>
       </div>
     );
   }
